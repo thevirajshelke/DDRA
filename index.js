e17c609ca616dab5fc06ac1b97400cf5cee51d34
virajs <viraj.shelke@merce.co>
2018-11-30 09:15:59 +0530

    Removed Normalize Port & added cmd args for passing port

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const app = require("./src/app");
 const http = require("http");
-const port = normalizePort(process.env.PORT || "8888");
+let port = "8888";
+
+process.argv.forEach(function (val) {
+    if (val.indexOf("--port=") !== -1) {
+        port = val.split("=")[1];
+    }
+});
 
 app.set("port", port);
-
 const server = http.createServer(app);
 
-function normalizePort(val) {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-        return val;
-    }
-    if (port >= 0) {
-        return port;
-    }
-    return false;
-}
-
 server.listen(port, () => {
-    console.log("The server is running on 8888...");
+    console.log("The server is running on port " + port + "...");
 });
