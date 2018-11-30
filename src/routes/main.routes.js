e1ce3e0ea0bba75bcb0c31c1795b2033b11fd63f
virajs <viraj.shelke@merce.co>
2018-11-30 18:40:08 +0530

    Started using utils.js function formatResponse()

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,81 +1,100 @@
 const express = require("express");
 const router = express.Router();
-const utils = require("../controllers/dns");
+const dns = require("../controllers/dns");
+const utils = require("../controllers/utils");
 
 router.get("/", async (req, res) => {
-    res.json({
-        data: "The server is up & running!"
-    });
+    res.json(utils.formatResponse("success", "The server is up & running!", null, null));
 });
 
 router.post("/resolveIPv4", async (req, res) => {
-    let resp = await utils.resolveIPv4(req.body.domain, req.body.ttl);
-    res.json({
-        data: resp
-    });
+    try {
+        let resp = await dns.resolveIPv4(req.body.domain, req.body.ttl);
+        res.json(utils.formatResponse("success", "Domain is resolved into IPv4 record(s)", resp, null));
+    } catch (err) {
+        res.json(utils.formatResponse("failure", "", null, err, "", null, err));
+    }
 });
 
 router.post("/resolveIPv6", async (req, res) => {
-    let resp = await utils.resolveIPv6(req.body.domain, req.body.ttl);
-    res.json({
-        data: resp
-    });
+    try {
+        let resp = await dns.resolveIPv6(req.body.domain, req.body.ttl);
+        res.json(utils.formatResponse("success", "Domain is resolved into IPv6 record(s)", resp, null));
+    } catch (err) {
+        res.json(utils.formatResponse("failure", "", null, err));
+    }
 });
 
 router.post("/resolveCNAME", async (req, res) => {
-    let resp = await utils.resolveCNAME(req.body.domain);
-    res.json({
-        data: resp
-    });
+    try {
+        let resp = await dns.resolveCNAME(req.body.domain);
+        res.json(utils.formatResponse("success", "Domain is resolved into CNAME record(s)", resp, null));
+    } catch (err) {
+        res.json(utils.formatResponse("failure", "", null, err));
+    }
 });
 
 router.post("/resolveMX", async (req, res) => {
-    let resp = await utils.resolveMX(req.body.domain);
-    res.json({
-        data: resp
-    });
+    try {
+        let resp = await dns.resolveMX(req.body.domain);
+        res.json(utils.formatResponse("success", "Domain is resolved into MX record(s)", resp, null));
+    } catch (err) {
+        res.json(utils.formatResponse("failure", "", null, err));
+    }
 });
 
 router.post("/resolveNS", async (req, res) => {
-    let resp = await utils.resolveNS(req.body.domain);
-    res.json({
-        data: resp
-    });
+    try {
+        let resp = await dns.resolveNS(req.body.domain);
+        res.json(utils.formatResponse("success", "Domain is resolved into NS record(s)", resp, null));
+    } catch (err) {
+        res.json(utils.formatResponse("failure", "", null, err));
+    }
 });
 
 router.post("/resolvePTR", async (req, res) => {
-    let resp = await utils.resolvePTR(req.body.domain);
-    res.json({
-        data: resp
-    });
+    try {
+        let resp = await dns.resolvePTR(req.body.domain);
+        res.json(utils.formatResponse("success", "Domain is resolved into PTR record(s)", resp, null));
+    } catch (err) {
+        res.json(utils.formatResponse("failure", "", null, err));
+    }
 });
 
 router.post("/resolveSOA", async (req, res) => {
-    let resp = await utils.resolveSOA(req.body.domain);
-    res.json({
-        data: resp
-    });
+    try {
+        let resp = await dns.resolveSOA(req.body.domain);
+        res.json(utils.formatResponse("success", "Domain is resolved into SOA record(s)", resp, null));
+    } catch (err) {
+        res.json(utils.formatResponse("failure", "", null, err));
+    }
 });
 
 router.post("/resolveSRV", async (req, res) => {
-    let resp = await utils.resolveSRV(req.body.domain);
-    res.json({
-        data: resp
-    });
+    try {
+        let resp = await dns.resolveSRV(req.body.domain);
+        res.json(utils.formatResponse("success", "Domain is resolved into SRV record(s)", resp, null));
+    } catch (err) {
+        res.json(utils.formatResponse("failure", "", null, err));
+    }
 });
 
 router.post("/resolveTXT", async (req, res) => {
-    let resp = await utils.resolveTXT(req.body.domain);
-    res.json({
-        data: resp
-    });
+    try {
+        let resp = await dns.resolveTXT(req.body.domain);
+        res.json(utils.formatResponse("success", "Domain is resolved into TXT record(s)", resp, null));
+    } catch (err) {
+        res.json(utils.formatResponse("failure", "", null, err));
+    }
 });
 
 router.post("/resolveANY", async (req, res) => {
-    let resp = await utils.resolveANY(req.body.domain);
-    res.json({
-        data: resp
-    });
+    try {
+        let resp = await dns.resolveANY(req.body.domain);
+        res.json(utils.formatResponse("success", "Domain is resolved into ANY record(s)", resp, null));
+    } catch (err) {
+        res.json(utils.formatResponse("failure", "", null, err));
+    }
 });
 
 // router.post("batchResolveMx", async (req, res) => {
