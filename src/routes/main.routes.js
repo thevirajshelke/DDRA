649a231b50fb99655d25d20092e30473bc01ac41
virajs <viraj.shelke@merce.co>
2018-11-30 08:59:47 +0530

    Added more routes for different DNS queries

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -1,19 +1,88 @@
 const express = require("express");
-const dnsPromises = require("dns").promises;
 const router = express.Router();
+const utils = require("../controllers/dns");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     res.json({
         data: "The server is up & running!"
     });
 });
 
-router.post("/resolveMx", async (req, res) => {
-    let resp = await dnsPromises.resolveMx(req.body.domain);
-    console.log(resp);
+router.post("/resolveIPv4", async (req, res) => {
+    let resp = await utils.resolveIPv4(req.body.domain, req.body.ttl);
     res.json({
         data: resp
-    })
+    });
 });
 
-module.exports = router
+router.post("/resolveIPv6", async (req, res) => {
+    let resp = await utils.resolveIPv6(req.body.domain, req.body.ttl);
+    res.json({
+        data: resp
+    });
+});
+
+router.post("/resolveCNAME", async (req, res) => {
+    let resp = await utils.resolveCNAME(req.body.domain);
+    res.json({
+        data: resp
+    });
+});
+
+router.post("/resolveMX", async (req, res) => {
+    let resp = await utils.resolveMX(req.body.domain);
+    res.json({
+        data: resp
+    });
+});
+
+router.post("/resolveNS", async (req, res) => {
+    let resp = await utils.resolveNS(req.body.domain);
+    res.json({
+        data: resp
+    });
+});
+
+router.post("/resolvePTR", async (req, res) => {
+    let resp = await utils.resolvePTR(req.body.domain);
+    res.json({
+        data: resp
+    });
+});
+
+router.post("/resolveSOA", async (req, res) => {
+    let resp = await utils.resolveSOA(req.body.domain);
+    res.json({
+        data: resp
+    });
+});
+
+router.post("/resolveSRV", async (req, res) => {
+    let resp = await utils.resolveSRV(req.body.domain);
+    res.json({
+        data: resp
+    });
+});
+
+router.post("/resolveTXT", async (req, res) => {
+    let resp = await utils.resolveTXT(req.body.domain);
+    res.json({
+        data: resp
+    });
+});
+
+router.post("/resolveANY", async (req, res) => {
+    let resp = await utils.resolveANY(req.body.domain);
+    res.json({
+        data: resp
+    });
+});
+
+// router.post("batchResolveMx", async (req, res) => {
+
+//     // res.json({
+//     //     data: resp
+//     // });
+// });
+
+module.exports = router;
